87c97fddca3a29c692419fc81fdb10d64d1f5e48
Lecter <leandro_valenzuela@hotmail.com>
2019-03-09 16:59:28 -0300

    MarketFND-v.0

---
+++ app/src/main/java/ergosoft/marketfnd/SesionIniciadaActivity.java
@@ -0,0 +1,13 @@
+package ergosoft.marketfnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SesionIniciadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sesion_iniciada);
+    }
+}
