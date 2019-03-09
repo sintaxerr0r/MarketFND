ddff16f9597b663197c579725f2e4218a563e771
Lecter <leandro_valenzuela@hotmail.com>
2019-03-08 22:35:52 -0300

    MarketFND-v.0

--- app/src/main/java/ergosoft/marketfnd/MainActivity.java
+++ app/src/main/java/ergosoft/marketfnd/MainActivity.java
@@ -2,12 +2,19 @@ package ergosoft.marketfnd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    // el basty se
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),
+                "El basty se la come",Toast.LENGTH_LONG).show();
     }
 }
