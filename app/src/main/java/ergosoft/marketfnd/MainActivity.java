afd63ad61719ab3f48a6f2b2dd1cd257b7b60e70
Lecter <leandro_valenzuela@hotmail.com>
2019-03-09 17:58:46 -0300

    MarketFND-v.0

--- app/src/main/java/ergosoft/marketfnd/MainActivity.java
+++ app/src/main/java/ergosoft/marketfnd/MainActivity.java
@@ -1,18 +1,26 @@
 package ergosoft.marketfnd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    // el basty se
+    //enlace registro
+    private TextView enlace;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enlace = findViewById(R.id.txtenlaceregistro);
+
 
 
     }
