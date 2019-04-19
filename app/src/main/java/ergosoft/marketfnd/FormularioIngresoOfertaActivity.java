20c5ff74cc1515f2916e26cd3f104059630a65f0
Lecter <leandro_valenzuela@hotmail.com>
2019-04-19 00:42:55 -0400

    MarKet

---
+++ app/src/main/java/ergosoft/marketfnd/FormularioIngresoOfertaActivity.java
@@ -0,0 +1,17 @@
+package ergosoft.marketfnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioIngresoOfertaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_ingreso_oferta);
+
+
+
+    }//fin oncreate
+
+}//fin class
