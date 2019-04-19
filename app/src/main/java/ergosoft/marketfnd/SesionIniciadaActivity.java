7e9e5d7aa786c1d355eb397fa9a1c9f55db17ef1
Bastian Vidal <bastianvidal1998@gmail.com>
2019-04-18 23:32:38 -0400

    implementando menu de navegacion lateral

--- app/src/main/java/ergosoft/marketfnd/SesionIniciadaActivity.java
+++ app/src/main/java/ergosoft/marketfnd/SesionIniciadaActivity.java
@@ -1,18 +1,36 @@
 package ergosoft.marketfnd;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import Clases.Usuario;
 
 public class SesionIniciadaActivity extends AppCompatActivity {
     Usuario objUsuario;
+    private String[] mPlanetTitles = {"Tierra","Marte","Sol"};
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesion_iniciada);
         objUsuario = getIntent().getExtras().getParcelable("objUsuario");
         Toast.makeText(this, objUsuario.getNombre(), Toast.LENGTH_SHORT).show();
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerList = findViewById(R.id.left_drawer);
+
+        //Set adapter
+        mDrawerList.setAdapter(new ArrayAdapter<String>
+                (this,R.layout.drawer_list_item,mPlanetTitles));
+        //Set Click Listener
+
+        // Click listener para el menu de navegacion
+        // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 }
